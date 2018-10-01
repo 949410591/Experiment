8ed22ce3dddf6d80784a3cb06a17455d2714156a
949410591 <33408761+949410591@users.noreply.github.com>
2018-10-02 02:16:02 +0800

    1

--- 8_week/shiyan26/simpledu/simpledu/forms.py
+++ 8_week/shiyan26/simpledu/simpledu/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import Length, Email, EqualTo, Required
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('用户名', validators=[Required(), Length(3,24)])
+    username = StringField('用户名', validators=[Required(), Length(3,24)])#第一个是映射的表单字段名字，后面是验证器
     email = StringField('邮箱', validators=[Required(), Email()])
     password = PasswordField('密码', validators=[Required(), Length(6,24)])
     repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo('password')])
